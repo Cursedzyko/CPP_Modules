7c9bb7a486fbcde0c72bc38a6ce1d6d7bacc2b92
Zafarbek Yunusov <zyunusov@c2s13d71.42wolfsburg.de>
2023-01-24 16:42:16 +0100

    Need to read about function oveloading

--- CPP_Module00/ex02/Account.cpp
+++ CPP_Module00/ex02/Account.cpp
@@ -6,9 +6,25 @@
 /*   By: zyunusov <zyunusov@student.42wolfsburg.de> +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/20 16:49:00 by zyunusov          #+#    #+#             */
-/*   Updated: 2023/01/20 16:49:26 by zyunusov         ###   ########.fr       */
+/*   Updated: 2023/01/24 14:11:53 by zyunusov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Account.hpp"
 
+int	Account::_nbAccounts = 0;
+int	Account::_totalAmount = 0;
+int	Account::_totalNbDeposits = 0;
+int	Account::_totalNbWithdrawals = 0;
+
+Account::Account(int initial_deposit)
+{
+    this->_accountIndex = this->_nbAccounts++;
+    this->_amount = 0;
+    this->_nbDeposits = 0;
+    this->_nbWithdrawals = 0;
+    Account::_displayTimestamp();
+    std::cout << "index:" << this->_accountIndex << ";";
+    std::cout << "amount: 0;";
+    std::cout << "created" << std::endl;
+}
